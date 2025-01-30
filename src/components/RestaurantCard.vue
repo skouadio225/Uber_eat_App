c3e817f0e2b010b03f91e319122c789e88febc0e
Konan Junior Kouadio <stephane.kouadio@grenoble-inp.org>
2025-01-30 11:46:16 +0100

    Installations et scss

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,28 @@
+<!-- ------------------ Template ------------------ -->
+<template>
+  <div class="restaurant--card">
+    <p> Hello World !! </p>
+
+  </div>
+</template>
+
+<!-- ------------------ Script ------------------ -->
+<script>
+export default {
+
+}
+</script>
+
+<!-- ------------------ Style ------------------ -->
+<style lang="scss">
+.restaurant--card {
+    width: 32;
+    height: 30vh;
+
+    p {
+        margin: 0px;
+    }
+}
+
+
+</style>
